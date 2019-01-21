e9c6e7fcbc1a28aada281c6fd57b51a476282033
Dan McAdams <danmcadams@gmail.com>
2019-01-21 13:59:31 -0600

    change theroadbunch/filter-lists to theroadbunch/bouncer

--- tests/Bouncer/WhitelistTest.php
+++ tests/Bouncer/WhitelistTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the theroadbunch/filter-lists package.
+ * This file is part of the theroadbunch/bouncer package.
  *
  * (c) Dan McAdams <danmcadams@gmail.com>
  *
